aafa9ed4c0e4374c1faf6f8aec3fac3cba3c00c7
CA-Lee (from PC) <email@calee.tw>
2021-01-22 07:06:08 +0000

    fix(config): use env

--- app/config-template.py
+++
@@ -1,6 +0,0 @@
-"""
-Copy/rename this file as config.py
-"""
-
-LINE_SECRET: str = "Channel secret"
-LINE_TOKEN: str = "Channel access token (long-lived)"
---
+++ app/config.py
@@ -0,0 +1,4 @@
+from os import environ
+
+LINE_SECRET: str = environ["LINE_SECRET"]
+LINE_TOKEN: str = environ["LINE_TOKEN"]
